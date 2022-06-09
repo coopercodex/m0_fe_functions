032c2e4a3ce63c03bce08b00bbe7163d05a5474c
Derek Cooper <derekcooper20@gmail.com>
2022-06-09 13:10:56 -0600

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,47 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
-}
+  console.log("Hello, what is your name?");
+};
 
-nameQuestion()
-
+nameQuestion();
+ // just added ; to make code easier to read
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 console.log(sum);
-      }
+};
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// just added spaces and ; to make code easier to read
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+};
 
 makeFreshPesto();
-
+// function was not spelled out code wont run.  Added ; and spaces to make it easier to read
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2)  {
 var sum = num1 + num2;
-    var avg = sum / 2;
+var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+  console.log(`the average is: ${avg}`);
+};
+
+average(1,2);
+// added/deleted spaces to make easier to read. Added call function and added arguments so code would run.
